91e79ee896d4884bf1cbcb612829660a1fe317c9
WoeOm <1015996366@qq.com>
2020-08-29 19:52:13 +0800

    Updates

---
+++ precache-manifest.f3937bb4a522fe7e098c96d9d81bcfac.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa38569239197e77d274afbab7620f2",
+    "url": "/static/media/banner.baa38569.png"
+  },
+  {
+    "revision": "a8bb5a416a4718d05a47",
+    "url": "/static/js/0.e6ec7ca6.chunk.js"
+  },
+  {
+    "revision": "31687839b87573087f50",
+    "url": "/static/js/1.724fc1cc.chunk.js"
+  },
+  {
+    "revision": "100d9db5de865a3f9a58d1c2606c50f2",
+    "url": "/static/media/partner-7.100d9db5.png"
+  },
+  {
+    "revision": "2ec64f6860b2ab3884d6",
+    "url": "/static/js/main.6ece0341.chunk.js"
+  },
+  {
+    "revision": "2226cbf9a95392fc8267",
+    "url": "/static/js/runtime~main.1c5a34cf.js"
+  },
+  {
+    "revision": "df44aea5e7638bb2a087",
+    "url": "/static/js/4.893dcd5a.chunk.js"
+  },
+  {
+    "revision": "73bbe608389535212dc03e6b7f35f47a",
+    "url": "/static/media/partner-3.73bbe608.png"
+  },
+  {
+    "revision": "a83f45ee482dd64d8c6d",
+    "url": "/static/js/5.ca389249.chunk.js"
+  },
+  {
+    "revision": "46c29a1bfbc522fe4f51",
+    "url": "/static/js/6.a5fceaa4.chunk.js"
+  },
+  {
+    "revision": "66832b56220330b62b1da6a66c7656c5",
+    "url": "/static/media/key-5-mobile.66832b56.png"
+  },
+  {
+    "revision": "f53eeaa5b0e93b07b032",
+    "url": "/static/js/7.cd756cf7.chunk.js"
+  },
+  {
+    "revision": "297b4e38fc87c6a503047c1cf45fa314",
+    "url": "/static/media/key-5.297b4e38.png"
+  },
+  {
+    "revision": "ff51ea0ad3cc7d92d3a2",
+    "url": "/static/js/8.9cb40523.chunk.js"
+  },
+  {
+    "revision": "895ac1617f24508f3918",
+    "url": "/static/js/9.4d65f2a0.chunk.js"
+  },
+  {
+    "revision": "e47853a6babeac4f1aefd0d92b396bfd",
+    "url": "/static/media/Rajdhani-Bold.e47853a6.ttf"
+  },
+  {
+    "revision": "199e3623690bf2235928af1c7cc69514",
+    "url": "/static/media/Rajdhani-SemiBold.199e3623.ttf"
+  },
+  {
+    "revision": "631e1a1e24e6254102cb78d90025bd79",
+    "url": "/static/media/news-eth-relay.631e1a1e.png"
+  },
+  {
+    "revision": "123dc5d84fb45b769028e37a37a8ced6",
+    "url": "/static/media/news-1.123dc5d8.jpg"
+  },
+  {
+    "revision": "d0aeb0bb8d2661056e509f28ee7378b5",
+    "url": "/static/media/news-2.d0aeb0bb.jpg"
+  },
+  {
+    "revision": "4f0f42e9ae99e1c33404e02994a600f5",
+    "url": "/static/media/lng.4f0f42e9.svg"
+  },
+  {
+    "revision": "8e5534e9c5265802cde98e32cc3244a3",
+    "url": "/static/media/github.8e5534e9.svg"
+  },
+  {
+    "revision": "54c01a1470336fdc2a677083bbc8e6eb",
+    "url": "/static/media/wx.54c01a14.jpg"
+  },
+  {
+    "revision": "f8c7905ecc52e0eabd3376d1edfe29cc",
+    "url": "/static/media/architecture-bg.f8c7905e.svg"
+  },
+  {
+    "revision": "3620e1355dfed3aa42fc4028587e021d",
+    "url": "/static/media/dev-team.3620e135.png"
+  },
+  {
+    "revision": "2ac3f5248da8f1525d3ed80b4ad45ccf",
+    "url": "/static/media/key-4-mobile.2ac3f524.png"
+  },
+  {
+    "revision": "e367b5ce05963f7bbdc725f72765ac00",
+    "url": "/static/media/architecture.e367b5ce.png"
+  },
+  {
+    "revision": "476229911bcc46bc75072721d9ef35e7",
+    "url": "/static/media/architecture-en.47622991.png"
+  },
+  {
+    "revision": "1c00267b11fa3996815566eae58075ac",
+    "url": "/static/media/architecture-mobile.1c00267b.png"
+  },
+  {
+    "revision": "e482ba082933d30e4110b15a57f7c2cb",
+    "url": "/static/media/architecture-mobile-en.e482ba08.png"
+  },
+  {
+    "revision": "14a4c822b1cded240d3aefebc6cffc16",
+    "url": "/static/media/architecture-solo.14a4c822.svg"
+  },
+  {
+    "revision": "6b86c66437164588d6c38a5d6505223d",
+    "url": "/static/media/architecture-chain.6b86c664.svg"
+  },
+  {
+    "revision": "cf8a7dd3b056614f6f8fa3dde564f054",
+    "url": "/static/media/economic.cf8a7dd3.png"
+  },
+  {
+    "revision": "ae7a638c6824303546fde2b807da1075",
+    "url": "/static/media/economic-en.ae7a638c.png"
+  },
+  {
+    "revision": "8eb5602d7877c78b719c550487b8999c",
+    "url": "/static/media/application-1.8eb5602d.png"
+  },
+  {
+    "revision": "53ac72e94e7ac49d93ce9c8a5bcc9f5b",
+    "url": "/static/media/application-2.53ac72e9.png"
+  },
+  {
+    "revision": "799880068b56c0a10ebbb2e11cd0c45c",
+    "url": "/static/media/roadmap.79988006.png"
+  },
+  {
+    "revision": "bcc296c7c94e980346c69438f9a69752",
+    "url": "/static/media/roadmap-m.bcc296c7.png"
+  },
+  {
+    "revision": "c7f550b89070877ba282c43188ba2348",
+    "url": "/static/media/key-1.c7f550b8.png"
+  },
+  {
+    "revision": "4be527d0903ccab34679d2e9a4a2404a",
+    "url": "/static/media/key-1-mobile.4be527d0.png"
+  },
+  {
+    "revision": "1a4de6d04e7e62d594e79f71422256ae",
+    "url": "/static/media/key-2.1a4de6d0.png"
+  },
+  {
+    "revision": "febd6042377ff77be2100fdf049fbef2",
+    "url": "/static/media/key-2-mobile.febd6042.png"
+  },
+  {
+    "revision": "eb663dee3f2f68aa9c57c7a69d6ae0ed",
+    "url": "/static/media/key-3.eb663dee.png"
+  },
+  {
+    "revision": "6bba768ca4ac4c57dca4c887484b0623",
+    "url": "/static/media/key-3-mobile.6bba768c.png"
+  },
+  {
+    "revision": "74ef9d2a1ec58d85be79751ef00939fd",
+    "url": "/static/media/key-4.74ef9d2a.png"
+  },
+  {
+    "revision": "2ec64f6860b2ab3884d6",
+    "url": "/static/css/main.8d19eca9.chunk.css"
+  },
+  {
+    "revision": "ff51ea0ad3cc7d92d3a2",
+    "url": "/static/css/8.d9724f0d.chunk.css"
+  },
+  {
+    "revision": "f53eeaa5b0e93b07b032",
+    "url": "/static/css/7.51b4885d.chunk.css"
+  },
+  {
+    "revision": "a83f45ee482dd64d8c6d",
+    "url": "/static/css/5.f36a9a55.chunk.css"
+  },
+  {
+    "revision": "31687839b87573087f50",
+    "url": "/static/css/1.18302d42.chunk.css"
+  },
+  {
+    "revision": "a5df1a22e77f197d07ea5ff80fd4629f",
+    "url": "/index.html"
+  }
+];
